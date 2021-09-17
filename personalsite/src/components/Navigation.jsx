beb90adca71512bfbe8ff248018dcdcb83a7da4b
Giuliana De Bellis <z5309331@unsw.edu.au>
2021-09-17 14:11:27 +1000

    Clean file structure

---
+++ personalsite/src/components/Navigation.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar>
+      <Container>
+        <Link to="/home" style={{ textDecoration: 'none' }}><Navbar.Brand><span style={{ fontSize:'30px' }}>Giuliana De Bellis</span></Navbar.Brand></Link>
+        <Navbar.Toggle />
+
+        <Navbar.Collapse className="justify-content-end">
+          <Nav.Link><Link to="/home" style={{ textDecoration: 'none' }}><span className="navbar-fnt">Home</span></Link></Nav.Link>
+          <Nav.Link><Link to="/blog" style={{ textDecoration: 'none' }}><span className="navbar-fnt">Blog</span></Link></Nav.Link>
+        </Navbar.Collapse>
+
+      </Container>
+      </Navbar>
+
+      <div className="horizontal-line"></div>
+    </>
+  );
+}
+
+export default Navigation;
